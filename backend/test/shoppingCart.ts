ff83ddb704fcf4c9d1680f04ea1f3c08e7baeee4
ZackJiangNoodoe <zackjiang@noodoe.com>
2022-03-25 15:24:06 +0800

    Add a unit test to test if we can get the correct number of products in the shopping cart

--- backend/test/shoppingCart.ts
+++ backend/test/shoppingCart.ts
@@ -1,9 +1,22 @@
 import { expect } from 'chai';
 import * as sinon from 'sinon';
 import { shoppingCartModel } from '../src/models/shoppingCart';
-import { addProduct, deleteProduct } from '../src/services/shoppingCart';
+import { products, addProduct, deleteProduct } from '../src/services/shoppingCart';
+import mongoose from "mongoose";
 
 describe('Shopping Cart Services', () => {
+
+  it('Should see a list of products', async ()=> {
+    sinon.stub(mongoose.Query.prototype, 'populate').resolves([
+      {
+        name: 'fake name'
+      }
+    ]);
+  
+    const result = await products();
+  
+    expect(result.length).to.equal(1);
+  });
 
   it('Should add product', async ()=> {
     
